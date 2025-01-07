53f467acef5d9cff8450c82f5bb7c607951ac13b
RahulKalekar <kalekarrahul12@gmail.com>
2025-01-07 23:26:50 +0530

    Day 1 completed

--- java_core/src/day1/ArrayCopy.java
+++ java_core/src/day1/ArrayCopy.java
@@ -18,7 +18,7 @@ public class ArrayCopy {
 
     public static void main(String[] args) {
 
-        int[] arr= {4,5,6,7,67};
+        int[] arr= {8,7,6,43,2};
 
         int[] copyOfArr=ArrayOp.copyOf(arr);
     }
