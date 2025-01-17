99dc39a1f306135ec9fe93d80ce13c28bbf878e2
RahulKalekar <kalekarrahul12@gmail.com>
2025-01-17 14:56:37 +0530

    day7 q2

---
+++ java_core/src/day7/AnnotatedClass.java
@@ -0,0 +1,17 @@
+package day7;
+
+@Author(name = "Johny")             // applying to class
+@Version(number = 1.0)
+public class AnnotatedClass {
+    @Author(name="Author1")             //for method
+    @Version(number=2.0f)
+    public void annotatedMethod1(){
+
+    }
+    @Author(name="Author2")
+    @Version(number=4.0f)
+    public void annotatedMethod2(){
+
+    }
+
+}
---
+++ java_core/src/day7/Version.java
@@ -0,0 +1,12 @@
+package day7;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value={ElementType.CONSTRUCTOR,ElementType.TYPE,ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Version {
+    double number();
+}
