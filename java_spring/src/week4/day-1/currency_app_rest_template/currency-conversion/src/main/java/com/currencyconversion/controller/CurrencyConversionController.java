f8b1ec6b2e7a9e2ec515ee46af6cc210606bbfb7
RahulKalekar <kalekarrahul12@gmail.com>
2025-02-05 15:39:10 +0530

    Rest template done

--- java_spring/src/week4/day-1/currency_app_rest_template/currency-conversion/src/main/java/com/currencyconversion/controller/CurrencyConversionController.java
+++ java_spring/src/week4/day-1/currency_app_rest_template/currency-conversion/src/main/java/com/currencyconversion/controller/CurrencyConversionController.java
@@ -1,13 +1,13 @@
 package com.currencyconversion.controller;
 
 import com.currencyconversion.dto.CurrencyConversionBean;
-import com.currencyconversion.serviceproxy.CurrencyExchangeServiceProxy;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
 
@@ -15,27 +15,24 @@ import java.math.BigDecimal;
 public class CurrencyConversionController {
 
 	private Logger logger = LoggerFactory.getLogger(this.getClass());
-	
+
 	@Autowired
-	private CurrencyExchangeServiceProxy proxy;
+	private RestTemplate restTemplate;
 
-	@GetMapping("/hello")
-	public String hello(){
-		return "hello";
-	}
-
-	//http://localhost:8100/currency-converter/from/USD/to/INR/quantity/10
+	// http://localhost:8100/currency-converter/from/USD/to/INR/quantity/10
 	@GetMapping("/currency-converter/from/{from}/to/{to}/quantity/{quantity}")
-	public CurrencyConversionBean convertCurrencyFeign(@PathVariable String from,
-													   @PathVariable String to,
-													   @PathVariable BigDecimal quantity) {
+	public CurrencyConversionBean convertCurrency(@PathVariable String from,
+												  @PathVariable String to,
+												  @PathVariable BigDecimal quantity) {
 
-		CurrencyConversionBean response = proxy.retrieveExchangeValue(from, to);
+		String url = "http://localhost:8000/currency-exchange/from/" + from + "/to/" + to;
+
+
+		CurrencyConversionBean response = restTemplate.getForObject(url, CurrencyConversionBean.class);
 
 		logger.info("{}", response);
 
 		return new CurrencyConversionBean(response.getId(), from, to, response.getConversionMultiple(), quantity,
 				quantity.multiply(response.getConversionMultiple()), response.getPort());
-
 	}
 }
